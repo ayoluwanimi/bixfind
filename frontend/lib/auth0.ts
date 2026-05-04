d5703e7529c24361ebf5870cdef8c83b929a0954
ayoluwanimi <66927901+ayoluwanimi@users.noreply.github.com>
2026-05-03 17:42:16 -0700

    Fix auth0.ts - remove JSX, use proper re-exports

--- frontend/lib/auth0.ts
+++ frontend/lib/auth0.ts
@@ -1,9 +1,8 @@
-'use client'
-
-import { useAuth0, Auth0Provider, withAuthenticationRequired, User } from '@auth0/auth0-react'
+import { Auth0Provider, withAuthenticationRequired, User } from '@auth0/auth0-react'
 import React from 'react'
 
-export { Auth0Provider, useAuth0, withAuthenticationRequired, User }
+export { Auth0Provider, useAuth0, withAuthenticationRequired } from '@auth0/auth0-react'
+export type { User } from '@auth0/auth0-react'
 
 export const isAuth0Configured = () => {
   return !!process.env.NEXT_PUBLIC_AUTH0_DOMAIN && !!process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID
@@ -14,29 +13,6 @@ export const auth0ProviderConfig = {
   clientId: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID || '',
   authorizationParams: {
     redirect_uri: typeof window !== 'undefined' ? window.location.origin : ''
-  }
-}
-
-export const AuthWrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  if (!isAuth0Configured()) {
-    return <>{children}</>
-  }
-  return (
-    <Auth0Provider {...auth0ProviderConfig}>
-      {children}
-    </Auth0Provider>
-  )
-}
-
-export const login = async () => {
-  if (typeof window !== 'undefined') {
-    const auth0 = await import('@auth0/auth0-react')
-  }
-}
-
-export const logout = () => {
-  if (typeof window !== 'undefined') {
-    window.location.href = `https://${auth0ProviderConfig.domain}/v2/logout?client_id=${auth0ProviderConfig.clientId}&returnTo=${encodeURIComponent(window.location.origin)}`
   }
 }
 
